3daa99280480f6476f875d580f8bd422b66f9c77
pratikshya-boop <pratikshya8848@gmail.com>
2026-01-08 16:54:23 +0545

    done login with paramaterization, searched the product, added to the cart and succesfully checkout

--- SaucelabAutomation/SauceLabs.py
+++ SaucelabAutomation/SauceLabs.py
@@ -31,6 +31,7 @@ wait = WebDriverWait(driver, 10)
 
 def click_element(driver, xpath):
     element = driver.find_element(By.XPATH, xpath)
+    driver.execute_script("arguments[0].scrollIntoView(true);", element)
     element.click()
 
 def send_keys_to_element(driver, xpath, keys):
@@ -38,31 +39,32 @@ def send_keys_to_element(driver, xpath, keys):
     element.clear()
     element.send_keys(keys)
 
-def clear_element(driver, xpath):
-    element = driver.find_element(By.XPATH, xpath)
-    element.clear()
+# def clear_element(driver, xpath):
+#     element = driver.find_element(By.XPATH, xpath)
+#     element.clear()
+
 
 #For sign up
-sign_up = "//a[normalize-space()='Sign up']"
-click_element(driver, sign_up)
-print("signup button is clicked")
-time.sleep(3)
+# sign_up = "//a[normalize-space()='Sign up']"
+# click_element(driver, sign_up)
+# print("signup button is clicked")
+# time.sleep(3)
 
 #Verify registration page opens properly by checking the url
-if "register" in driver.current_url:
-    print("Registration form loaded successfully")
-else:
-    print("Warning: Registration form may not have loaded properly - current URL:", driver.current_url)
-time.sleep(2)
-first_name = "//input[@id='first_name']"
-send_keys_to_element(driver, first_name, "Pratikshya")
-print("First name is entered")
-time.sleep(3)
-last_name = "//input[@id='last_name']"
-send_keys_to_element(driver, last_name, "Bhattarai")
-print("Last name is entered")
-time.sleep(3)
-email_add = "//input[@id='email']"
+# if "register" in driver.current_url:
+#     print("Registration form loaded successfully")
+# else:
+#     print("Warning: Registration form may not have loaded properly - current URL:", driver.current_url)
+# time.sleep(2)
+# first_name = "//input[@id='first_name']"
+# send_keys_to_element(driver, first_name, "Pratikshya")
+# print("First name is entered")
+# time.sleep(3)
+# last_name = "//input[@id='last_name']"
+# send_keys_to_element(driver, last_name, "Bhattarai")
+# print("Last name is entered")
+# time.sleep(3)
+# email_add = "//input[@id='email']"
 # Generate a unique email address each time
 # time.time() comes from python's built-in time module
 # It returns the current time in seconds since jan 1, 1970
@@ -71,55 +73,107 @@ email_add = "//input[@id='email']"
 # "testuser_{int(time.time())}@gmail.com" The timestamp is inserted into the email string
 
 
-unique_email = f"testuser_{int(time.time())}@gmail.com"
-send_keys_to_element(driver, email_add, unique_email)
-print("Email is entered")
-
-password = "//input[@id='password']"
-send_keys_to_element(driver, password, "Pratik@123")
-print("Password is entered")
-
-create_acc = "//input[@value='Create']"
-click_element(driver, create_acc)
+# unique_email = f"testuser_{int(time.time())}@gmail.com"
+# send_keys_to_element(driver, email_add, unique_email)
+# print("Email is entered")
+#
+# password = "//input[@id='password']"
+# send_keys_to_element(driver, password, "Pratik@123")
+# print("Password is entered")
+#
+# create_acc = "//input[@value='Create']"
+# click_element(driver, create_acc)
 
 #Assertion for sign up
-logout_btn_element = driver.find_element(By.XPATH, "//a[text()='Log Out']")
-if logout_btn_element:
-    print("Registration successful - log out element found")
-else:
-    print("Registration not succesful - log out element not found")
+# logout_btn_element = driver.find_element(By.XPATH, "//a[text()='Log Out']")
+# if logout_btn_element:
+#     print("Registration successful - log out element found")
+# else:
+#     print("Registration not succesful - log out element not found")
 
-#For login
-login_btn = "//a[normalize-space()='Log In']"
-click_element(driver, login_btn)
 
-login_email = "//input[@id='customer_email']"
-send_keys_to_element(driver, login_email, "sabitra231@gmail.com")
+test_search_data = [
+    # {
+    #     "email": "pratik@gmail.com",
+    #     "password": "Hello",
+    #     "search_keyword": "jacket"
+    # },
+    # {
+    #     "email": "pratikshya@gmail.com",
+    #     "password": "Hello@123",
+    #     "search_keyword": "Shirt"
+    # },
+    {
+        "email": "sabitra231@gmail.com",
+        "password": "Pratik@123",
+        "search_keyword": "jacket"
 
-login_password = "//input[@id='customer_password']"
-send_keys_to_element(driver, login_password, "Pratik@123")
+    }
+]
 
-signin_btn = "//input[@value='Sign In']"
-click_element(driver, signin_btn)
+for i, user_data in enumerate(test_search_data):
+    driver.get("https://sauce-demo.myshopify.com")
+
+    #For login
+    # login_btn = "//a[normalize-space()='Log In']"
+    # click_element(driver, login_btn)
+    #
+    # login_email = "//input[@id='customer_email']"
+    # send_keys_to_element(driver, login_email, user_data["email"])
+    #
+    # login_password = "//input[@id='customer_password']"
+    # send_keys_to_element(driver, login_password, user_data["password"])
+    #
+    # signin_btn = "//input[@value='Sign In']"
+    # click_element(driver, signin_btn)
+
+#For search
+search_field = "//input[@id='search-field']"
+send_keys_to_element(driver, search_field, "Grey Jacket")
+
+time.sleep(2)
+
+ #For add to cart
+#first select product
+click_product = "//a[@id='product-1']"
+click_element(driver, click_product)
+
+#Now add the product to cart
+add_to_cart = "//input[@id='add']"
+click_element(driver, add_to_cart)
+
+time.sleep(3)
+
+#Now check out
+check_out = "//a[@class='checkout']"
+click_element(driver, check_out)
+
+#Now checkout after going in cart
+check_out_btn = "//input[@id='checkout']"
+click_element(driver, check_out_btn)
+time.sleep(30)
+
+driver.quit()
+
 
 #For My Account
-my_account = "//a[@href='/account'][normalize-space()='My Account']"
-click_element(driver, my_account)
+# my_account = "//a[@href='/account'][normalize-space()='My Account']"
+# click_element(driver, my_account)
 
 #For about us
-about_us = "//div[@class='seven columns offset-by-one desktop']//a[normalize-space()='About Us']"
-click_element(driver, about_us)
+# about_us = "//div[@class='seven columns offset-by-one desktop']//a[normalize-space()='About Us']"
+# click_element(driver, about_us)
 
 # Verify that we are in about us page
-if "about-us" in driver.current_url:
-    print("We are in about us page")
+# if "about-us" in driver.current_url:
+#     print("We are in about us page")
+#
+# else:
+#     print("We are not in about us page")
 
-else:
-    print("We are not in about us page")
 
 
 
-time.sleep(30)
 
 
 
